81d1d0ef864b3a491e9516a1623f26b226d0380f
Caio César Ferreira <caiocesar390@dinfor.uniceub>
2025-03-17 10:29:06 -0300

    Implementando funcionalidade de calculadora simples

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post" action= "resultado.php">
+        Numero1:<input type= "number" name="numero1">
+        <br><br>
+        Numero2:<input type= "number" name="numero2">
+        <br><br>
+        <input type="submit" value="Soma">
+        <input type="reset" value="Limpar">
+    </form>
+    
+</body>
+</html>
---
+++ resultado.php
@@ -0,0 +1,11 @@
+<?php
+
+    $numero1 = $_REQUEST['numero1'];
+    $numero2 = $_REQUEST['numero2'];
+
+     $b = "Resultado:";
+    
+    echo $b . $numero1 + $numero2;
+
+
+?>
